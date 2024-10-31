060a0e0349242777f0263faf1670cee75d818e02
GANTEDA ARAVIND <aravindganteda@gmail.com>
2024-10-31 14:54:27 +0530

    scroll progess update

--- frontend/src/components/ScrollProgessBar.tsx
+++ frontend/src/components/ScrollProgessBar.tsx
@@ -1,24 +1,22 @@
 import { motion, useScroll } from "framer-motion";
-const ScrollProgessBar = () => {
+
+const ScrollProgressBar = () => {
   const { scrollYProgress } = useScroll();
+
   return (
-    <div className={"h-1 w-full bg-purple-500"}>
+    <div className="h-1 w-full bg-transparent fixed top-0 left-0 z-1000">
       <motion.div
         style={{
-          scaleX: scrollYProgress, // Scale horizontally with scroll progress
-          transformOrigin: "0 0", // Scale from the left
-          height: "4px", // Adjust thickness of the progress bar
-          color: "#7E22CE", // Progress bar color
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
+          scaleX: scrollYProgress,        // Scale horizontally with scroll progress
+          transformOrigin: "0 0",         // Scale from the left
+          width: "100%",                  // Set width for the scaling effect
+          height: "4px",                  // Adjust thickness of the progress bar
+          backgroundColor: "#7E22CE",     // Progress bar color
           borderRadius: 100,
-          zIndex: 1000,
         }}
       />
     </div>
   );
 };
 
-export default ScrollProgessBar;
+export default ScrollProgressBar;
