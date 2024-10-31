e0e2a04305011e86645fffe7b41583e560fa7f7e
GANTEDA ARAVIND <aravindganteda@gmail.com>
2024-10-31 14:43:52 +0530

    removing un nessacary imports

--- frontend/src/components/ScrollProgessBar.tsx
+++ frontend/src/components/ScrollProgessBar.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+
+import { motion, useScroll } from "framer-motion";
 const ScrollProgessBar = () => {
   const { scrollYProgress } = useScroll();
   return (
-    <div className = {"h-1 w-full"}>
+    <div className = {"h-1 w-full bg-purple-500"}>
       <motion.div style={{ scaleX: scrollYProgress }} />
     </div>
   );
