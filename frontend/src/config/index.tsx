bbee3ad71294b9730f4f6d0c0303ba2b7482477b
GANTEDA ARAVIND <aravindganteda@gmail.com>
2024-11-09 21:47:35 +0530

    adding vercel api

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of Axios
 const axiosInstance = axios.create({
-  baseURL: 'https://blogbazzarapi.onrender.com/api/v1',  // Set the base URL for all requests
+  baseURL: 'https://blogbazzarapi.vercel.app/api/v1',  // Set the base URL for all requests
   timeout: 180000,  // Set a default timeout (10 seconds)
   headers: {
     'Content-Type': 'application/json',  // Default content type
